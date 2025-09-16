496184a5ae70dde253fac0929dabb5bf158017ed
Hariom Rai <hariomrai8252@gmail.com>
2025-09-16 18:44:32 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,10 @@
+// Narrowing example in Java
+public class Narrowing {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble;
+
+        System.out.println(myDouble); // 9.78
+        System.out.println(myInt);    // 9
+    }
+}

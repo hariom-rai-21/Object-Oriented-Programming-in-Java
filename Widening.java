12c04b2d79484cb92df1ac30108f07e84aae2ab5
Hariom Rai <hariomrai8252@gmail.com>
2025-09-16 18:46:01 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,10 @@
+// Widening example in Java
+public class Widening {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);    // 9
+        System.out.println(myDouble); // 9.0
+    }
+}
